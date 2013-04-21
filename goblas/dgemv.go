dd209bf5b439b8399f6a38947bbe3ca3cfb5d29e
David Neumann <david_neumann@gmx.net>
2013-04-21 22:12:54 +0200

    added license

--- goblas/dgemv.go
+++ goblas/dgemv.go
@@ -6,4 +6,16 @@ import "github.com/kortschak/blas"
 func (Blas) Dgemv(o blas.Order, tA blas.Transpose, m, n int, alpha float64, a []float64,
 	lda int, x []float64, incX int, beta float64, y []float64, incY int) {
 
+	dscal(m, beta, y, incY)
+	if (o == blas.ColMajor && tA == blas.NoTrans) ||
+		(o == blas.RowMajor && tA == blas.Trans) {
+		for c := 0; c < n; c++ {
+			daxpy(m, alpha*x[incX*c], a[c*lda:], 1, y, incY)
+		}
+	} else {
+		for r := 0; r < m; r++ {
+			y[r*incY] += alpha * ddot(n, a[r*lda:], 1, x, incX)
+		}
+	}
+
 }
--- goblas/dswap_amd64.s
+++ goblas/dswap_amd64.s
@@ -1,3 +1,30 @@
+/*
+Copyright (c) 2011, Michal Derkacz
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions
+are met:
+1. Redistributions of source code must retain the above copyright
+   notice, this list of conditions and the following disclaimer.
+2. Redistributions in binary form must reproduce the above copyright
+   notice, this list of conditions and the following disclaimer in the
+   documentation and/or other materials provided with the distribution.
+3. The name of the author may not be used to endorse or promote products
+   derived from this software without specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR
+IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT,
+INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT
+NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
+THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*/
+
 // 6g -B: 1926 ns/op, 6a MOVUPD: 2397 ns/op, 6a MOVAPD: 474 ns/op
 // MOVUPD is very inefficient on my U4100. Can it work better on other CPU?
 
